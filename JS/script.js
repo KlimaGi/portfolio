d66a7dac8f23e8e2b3fb5aadf88cf4eb355d480b
Giedre Klimaite <giedre.klima@gmail.com>
2020-11-12 23:49:21 +0200

    add animations

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const madedLogo = document.querySelector(".madedLogo");
+const menu = document.querySelector(".menu");
+const headline = document.querySelector(".headline");
+
+const t1 = new TimelineMax();
+t1.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
+// box lines style
 function scrollAppear(){
   var introText = document.querySelector('.intro_text');
   var introPosition = introText.getBoundingClientRect().top;
